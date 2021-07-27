8717eedc9249f578960a85325e168f5b390456d3
sonalkeraliya <dalsaniya.sonal46@gmail.com>
2021-07-28 01:29:17 +1000

    calculate contoller for getting request data and calacuate day and weeks

---
+++ app/Http/Controllers/calculateController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController as BaseController;
+use Validator;
+use Carbon\Carbon; 
+
+class calculateController extends BaseController
+{
+    function getData(Request $request){
+        
+    }
+}
