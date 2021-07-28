3e2a7f18d07702354f9526935218f6ffafdb0c4c
sonalkeraliya <dalsaniya.sonal46@gmail.com>
2021-07-28 10:30:36 +1000

    Define all test cases in laravel unit tescase file

--- tests/Feature/CalcDayWeekAPITest.php
+++ tests/Feature/CalcDayWeekAPITest.php
@@ -9,14 +9,65 @@ use Tests\TestCase;
 class CalcDayWeekAPITest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * check required all data
      *
      * @return void
      */
-    public function test_example()
+    public function testAllRequestdataRequired()
     {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+
     }
+    /**
+     * check datetime proper format for fromdate param
+     *
+     * @return void
+     */
+    public function testcheckFromdateFormat()
+    {
+
+
+    }
+  
+      /**
+     * check datetime proper format for to_date param
+     *
+     * @return void
+     */
+    public function testcheckTodateFormat()
+    {
+
+
+    }
+    
+      /**
+     * check timezone proper format for from_timezone param
+     *
+     * @return void
+     */
+    public function testcheckFromTimezoneFormat()
+    {
+
+
+    }
+        /**
+       * check  timezone proper format for to_timezone param
+       *
+       * @return void
+       */
+      public function testcheckToTimezoneFormat()
+      {
+
+
+      }
+        /**
+       * check  proper return calculate days and weeks
+       *
+       * @return void
+       */
+      public function testcheckCalDaysWeeks()
+      {
+
+
+      }
 }
