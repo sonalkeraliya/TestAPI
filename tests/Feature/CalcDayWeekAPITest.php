f1c42564ebfda9813653712653f45e93a170c102
sonalkeraliya <dalsaniya.sonal46@gmail.com>
2021-07-28 10:50:34 +1000

    check assert data for all test cases

--- tests/Feature/CalcDayWeekAPITest.php
+++ tests/Feature/CalcDayWeekAPITest.php
@@ -15,7 +15,8 @@ class CalcDayWeekAPITest extends TestCase
      */
     public function testAllRequestdataRequired()
     {
-
+       $response = $this->call('POST', '/GetCntDaysWeeks', ['from_date' => '','to_date' => '','from_timezone' => '','to_timezone' => '']);
+       $this->assertEquals(404, $response->status());
 
     }
     /**
@@ -25,7 +26,11 @@ class CalcDayWeekAPITest extends TestCase
      */
     public function testcheckFromdateFormat()
     {
-
+      $this->post('api/GetCntDaysWeeks',['from_date'=>'21/11/11','to_date' => '2021-01-01 00:00:00',
+      'from_timezone' => 'Africa/Conakry','to_timezone' => 'Africa/Dakar'])
+      ->assertStatus(404)
+      ->assertExactJson(['message'=>'Validation Error.','success'=>false,
+      'data' => ['from_date' => array ( 0 => 'The from date does not match the format Y-m-d H:i:s.')]]) ;
 
     }
   
@@ -36,7 +41,13 @@ class CalcDayWeekAPITest extends TestCase
      */
     public function testcheckTodateFormat()
     {
-
+      $this->post('api/GetCntDaysWeeks',['from_date'=>'2021-01-01 00:00:00','to_date' => '21/11/11',
+      'from_timezone' => 'Africa/Conakry','to_timezone' => 'Africa/Dakar'])
+      ->assertStatus(404)
+      ->assertExactJson(['message'=>'Validation Error.','success'=>false,
+      'data' => ['to_date' =>
+         array ( 0 => 'The to date does not match the format Y-m-d H:i:s.')]
+        ]) ;
 
     }
     
@@ -47,7 +58,12 @@ class CalcDayWeekAPITest extends TestCase
      */
     public function testcheckFromTimezoneFormat()
     {
-
+      $this->post('api/GetCntDaysWeeks',['from_date'=>'2021-01-01 00:00:00','to_date' => '2021-01-01 00:00:00','from_timezone' => 'Africa','to_timezone' => 'Africa/Conakry'])
+      ->assertStatus(404)
+      ->assertExactJson(['message'=>'Validation Error.','success'=>false,
+      'data' => ['from_timezone' =>
+         array ( 0 => 'The from timezone must be a valid timezone.')]
+        ]) ;
 
     }
         /**
@@ -57,7 +73,13 @@ class CalcDayWeekAPITest extends TestCase
        */
       public function testcheckToTimezoneFormat()
       {
-
+        
+        $this->post('api/GetCntDaysWeeks',['from_date'=>'2021-01-01 00:00:00','to_date' => '2021-01-01 00:00:00','from_timezone' => 'Africa/Conakry','to_timezone' => 'Africa'])
+      ->assertStatus(404)
+      ->assertExactJson(['message'=>'Validation Error.','success'=>false,
+      'data' => ['to_timezone' =>
+         array ( 0 => 'The to timezone must be a valid timezone.')]
+        ]) ;
 
       }
         /**
@@ -67,7 +89,13 @@ class CalcDayWeekAPITest extends TestCase
        */
       public function testcheckCalDaysWeeks()
       {
-
+        // all data valid pass and check days and weeks proper
+        $this->post('api/GetCntDaysWeeks',['from_date'=>'2020-01-01 00:00:00','to_date' => '2021-01-01 00:00:00',
+        'from_timezone' => 'Africa/Conakry','to_timezone' => 'Africa/Dakar'])
+        ->assertStatus(200)
+        ->assertExactJson(['success'=>true,'message'=>'Successfully response',
+        'data' => ['hours' => 366, 'weeks'=> 52   ]
+          ]) ;
 
       }
 }
